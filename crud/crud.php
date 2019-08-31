8a96a67a4120da2533a846d2542cb6ccc3885523
unknown <aprirandii@gmail.com>
2019-08-31 21:11:07 +0700

    Memasukan file ke git

---
+++ crud/crud.php
@@ -0,0 +1,11 @@
+<?php
+
+$conn = new mysqli("localhost", "root", "", "crud");
+
+// //code to save user's data
+// if ($conn) {
+// 	echo "Sukses Koneksi";
+// }else {
+// 	echo "Tidak Terkoneksi";
+// }
+
